c9d7ead7deed2dd6bf9bfe8aa62485f8e7187f52
renmet28 <ren_met@yahoo.com>
2021-09-28 13:45:03 -0400

    doodle

--- client/doodle/script.js
+++ client/doodle/script.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const doodler = document.createElement('div')
     let doodlerLeftSpace = 50
     let doodlerBottomSpace = 150
+    let isGameOver = false
     let platformCount = 5
+    let plaforms = []
 
     function createDoodler() {
         grid.appendChild(doodler)
@@ -31,6 +33,8 @@ document.addEventListener('DOMContentLoaded', () => {
             let platGap = 600 / platformCount
             let newPlatBottom = 100 + i * platGap
             let newPlatform = new Platform(newPlatBottom)
+            platforms.push(newPlatform)
+            console.log(platforms)
 
         }
     }
