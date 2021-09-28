e748c0442eb77b38fa4bbb47ccdd05be52cf521b
renmet28 <ren_met@yahoo.com>
2021-09-28 13:05:07 -0400

    dodle

--- client/doodle/script.js
+++ client/doodle/script.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const doodler = document.createElement('div')
+    let doodlerLeftSpace = 50
 
     function createDoodler() {
         grid.appendChild(doodler)
         doodler.classList.add('doodler')
-        doodler.style.left = '50px';
+        doodler.style.left = doodlerLeftSpace + 'px'
     }
     createDoodler()
 })
