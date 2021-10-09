0183a69e6d5df1814c36bc46433fd093dec22938
renmet28 <ren_met@yahoo.com>
2021-10-08 22:59:21 -0400

    lab6

---
+++ client/lab_6/script.js
@@ -0,0 +1,10 @@
+async function windowActions() {
+  const request = await fetch(https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json)
+  const arrayName = await request.json()
+}
+
+function displayMatches(event) {
+  event.target.value
+}
+
+window.onload = windowActions;
